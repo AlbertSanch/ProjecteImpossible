104b092164158814f93c1a51d7543124ed5baa88
AlbertSanch <albertsanchezguiu@gmail.com>
2021-12-03 13:11:41 +0100

    roger

--- app/src/main/java/com/example/projecteimpossible/activity_restaurant.java
+++ app/src/main/java/com/example/projecteimpossible/activity_restaurant.java
@@ -1,29 +1,91 @@
 package com.example.projecteimpossible;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class activity_restaurant extends AppCompatActivity implements Fragmentcinco.OnFragmentInteractionListener{
-    Fragmentcinco Fragment5;
+import java.util.ArrayList;
+import java.util.Objects;
 
+public class activity_restaurant extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Spinner spinner;
+    private final ArrayList<String> lista = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        String[] letra = {"TRIA VALORACIÓ","5 ESTRELLES","4 ESTRELLES","3 ESTRELLES","2 ESTRELLES","1 ESTRELLES"};
-        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, letra));
+
+        spinner = findViewById(R.id.spinner);
+        crearlista();
+        clickat();
+
+
+
     }
+    private void clickat(){
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,lista);///llista array
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);///perque es desplegui aball
+        spinner.setAdapter(adapter);
+    }
+    private void crearlista(){
+        lista.add("5 ESTRELLAS");
+        lista.add("4 ESTRELLAS");
+        lista.add("3 ESTRELLAS");
+    }
+
+
     public void onclick(View view) {
 
-            Intent i1 = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(i1);
+        Intent i1 = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i1);
+
+        }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String valor;
+        valor = (String) adapterView.getSelectedItem();
+        FragmentManager fm1 = getSupportFragmentManager();
+        Fragmentcinco fragment1 = new Fragmentcinco();
+        FragmentManager fm2 = getSupportFragmentManager();
+        Fragmentcuatro fragment2 = new Fragmentcuatro();
+        FragmentManager fm3 = getSupportFragmentManager();
+        Fragmenttres fragment3 = new Fragmenttres();
+        switch (valor){
+            case "5 ESTRELLAS":
+                if (getSupportFragmentManager().findFragmentById(R.id.container)!=null){
+                    getSupportFragmentManager().beginTransaction().remove(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.container))).commit();
+                }
+                fm1.beginTransaction().add(R.id.container,fragment1).commit();
+                break;
+            case "4 ESTRELLAS":
+                if (getSupportFragmentManager().findFragmentById(R.id.container)!=null){
+                    getSupportFragmentManager().beginTransaction().remove(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.container))).commit();
+                }
+                fm2.beginTransaction().add(R.id.container,fragment2).commit();
+                break;
+            case "3 ESTRELLAS":
+                if (getSupportFragmentManager().findFragmentById(R.id.container)!=null){
+                    getSupportFragmentManager().beginTransaction().remove(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.container))).commit();
+                }
+                fm3.beginTransaction().add(R.id.container,fragment3).commit();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
